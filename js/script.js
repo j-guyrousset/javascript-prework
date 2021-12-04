f90016ab5100fcaa89e9d573c2af2adccb95b0e7
Jean-Guy Rousset <rousset.jean-guy@laposte.net>
2021-12-04 21:46:30 +0100

    update script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let computerMove = 'stone';
+let playerMove = 'paper';
+
+printMessage('I myself have played: ' + computerMove + 'You miserable have
+played: ' + playerMove. + 'This you have been lucky... I claim for REVENGE!');
