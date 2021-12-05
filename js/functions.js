eecacb3260767c557244b957eb4d238a4fc64a50
Jean-Guy Rousset <rousset.jean-guy@laposte.net>
2021-12-05 12:48:34 +0100

    update script.js with functions

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,38 @@ function printMessage(msg){
 function clearMessages(){
   document.getElementById('messages').innerHTML='';
 }
+
+
+function getMoveName(num){
+  console.log('function getMove used');
+  let move = 'unknown choice, please try again.';
+  if (num == 1){
+    move = 'paper';
+  } else if (num == 2) {
+    move = 'stone';
+  } else if (num == 3) {
+    move = 'scissors';
+  }
+
+  return move;
+}
+
+
+function getResult(compMove, playMove){
+  console.log('fuction getResult used.');
+  let game = compMove + '-' + playMove;
+  let result = "undefined choice, try again but don't you cheat me...";
+  console.log(game);
+
+  if (compMove == playMove){
+    result = "I chose " + compMove + " too, let's play again!";
+    console.log("draw");
+  } else if (game == 'paper-stone' || game == 'scissors-paper' || game == 'stone-scissors'){
+    result = "I have " + compMove + " you have " + playMove + ". I win, you loose.";
+    console.log('computer wins.');
+  } else if (game == 'stone-paper' || game == 'paper-scissors' || game == 'scissors-stone') {
+    result = "I have " + compMove + " you have " + playMove + ". You win this time, let's play again!";
+    console.log('player wins.');
+  }
+  return printMessage(result);
+}
--- js/script.js
+++ js/script.js
@@ -1,61 +1,11 @@
 let randomNumber = Math.floor(Math.random()*3+1);
-let computerMove = '';
-
-if (randomNumber == 1){
-  computerMove = 'paper';
-} else if (randomNumber == 2) {
-  computerMove = 'stone';
-} else if (randomNumber == 3) {
-  computerMove = 'scisors';
-}
+let computerMove = getMoveName(randomNumber);
 console.log('computer random nuber is: ' + randomNumber +
 ' - computer move is: ' + computerMove);
 
-let playerInput = prompt("Please make your choice! 1: stone, 2: paper, 3: scisors. I am waiting...");
-let playerMove = 'unknown choice, try again!';
-console.log();
-
-if (playerInput == 1){
-  playerMove = 'stone';
-} else if (playerInput == 2) {
-  playerMove = 'paper';
-} else if (playerInput == 3) {
-  playerMove = 'scisors';
-} else {
-  printMessage(playerMove);
-}
+let playerInput = prompt("Please make your choice! 1: paper, 2: stone, 3: scissors. I am waiting...");
+let playerMove = getMoveName(playerInput);
 console.log('player choice: ' + playerInput +
 ' - player move is: ' + playerMove);
 
-if (computerMove == playerMove){
-  printMessage('I chose '+ computerMove + ' too. Ex aequo this time!');
-  printMessage('You are a tough player... try again!');
-}
-
-if (computerMove == 'paper'){
-  if (playerMove == 'stone'){
-    printMessage('paper wraps stone - I win you loose!');
-  } else if (playerMove == 'scisors') {
-    printMessage("scisors cut paper - you win, this time. Let's play again!");
-  } else {
-    printMessage("And don't try to cheat me this time!");
-  }
-
-} else if (computerMove == 'stone') {
-  if (playerMove == 'paper'){
-    printMessage("paper wraps stone - you win, this time. Let's play again!");
-  } else if (playerMove == 'scisors') {
-    printMessage('Stone breaks scisors - I win, you loose!');
-  } else {
-    printMessage("And don't try to cheat me this time!");
-  }
-  
-} else if (computerMove == 'scisors') {
-  if (playerMove == 'paper'){
-    printMessage("scisors cut paper - I win you loose!");
-  } else if (playerMove == 'stone') {
-    printMessage("Stone breaks scisors - you win, this time. Let's play again!");
-  } else {
-    printMessage("And don't try to cheat me this time!");
-  }
-}
+getResult(computerMove, playerMove);
