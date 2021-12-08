5ac3cb209fa115f63f502a6e5620c0194dbb6be6
Jean-Guy Rousset <rousset.jean-guy@laposte.net>
2021-12-08 10:21:40 +0100

    update script.js and function.js

--- js/functions.js
+++ js/functions.js
@@ -1,67 +1,69 @@
-let compCount = 0;
-let playerCount = 0;
+{
+  let compCount = 0,
+   playerCount = 0;
 
-function printMessage(msg){
-  let div = document.createElement('div');
-  div.innerHTML = msg;
-  document.getElementById('messages').appendChild(div);
-}
-
-
-function printCountById(msg, id){
-  document.getElementById(id).innerHTML='';
-  let div = document.createElement('div');
-  div.innerHTML = msg;
-  document.getElementById(id).appendChild(div);
-}
-
-
-function clearMessages(){
-  document.getElementById('messages').innerHTML='';
-}
-
-
-function resetCount(){
-  clearMessages();
-  compCount = 0;
-  playerCount = 0;
-  printCountById(compCount, 'comp-count');
-  printCountById(compCount, 'player-count');
-}
-
-
-function getMoveName(num){
-  let move = 'unknown choice, please try again.';
-  if (num == 1){
-    move = 'paper';
-  } else if (num == 2) {
-    move = 'stone';
-  } else if (num == 3) {
-    move = 'scissors';
+  function printMessage(msg){
+    const div = document.createElement('div');
+    div.innerHTML = msg;
+    document.getElementById('messages').appendChild(div);
   }
 
-  return move;
-}
+
+  function printCountById(msg, id){
+    document.getElementById(id).innerHTML='';
+    const div = document.createElement('div');
+    div.innerHTML = msg;
+    document.getElementById(id).appendChild(div);
+  }
 
 
-function getResult(compMove, playMove){
-  let game = compMove + '-' + playMove;
-  let result = "undefined choice, try again but don't you cheat me...";
-  console.log('computer - player: ' + game);
+  function clearMessages(){
+    document.getElementById('messages').innerHTML='';
+  }
 
-  if (compMove == playMove){
-    result = "I chose " + compMove + " too, let's play again!";
-    console.log("draw");
-  } else if (game == 'paper-stone' || game == 'scissors-paper' || game == 'stone-scissors'){
-    result = "I have " + compMove + ", you have " + playMove + ". I win, you loose.";
-    compCount += 1;
-    console.log('computer wins - score: ' + compCount);
+
+  function resetCount (){
+    clearMessages();
+    compCount = 0;
+    playerCount = 0;
     printCountById(compCount, 'comp-count');
-  } else if (game == 'stone-paper' || game == 'paper-scissors' || game == 'scissors-stone'){
-    result = "I have " + compMove + ", you have " + playMove + ". You win this time, let's play again!";
-    playerCount += 1;
-    console.log('player wins - score: ' + playerCount);
-    printCountById(playerCount, 'player-count');
+    printCountById(compCount, 'player-count');
   }
-  return printMessage(result);
+
+
+  function getMoveName(num){
+    let move = 'unknown choice, please try again.';
+    if (num == 1){
+      move = 'paper';
+    } else if (num == 2) {
+      move = 'stone';
+    } else if (num == 3) {
+      move = 'scissors';
+    }
+
+    return move;
+  }
+
+
+  function getResult(compMove, playMove){
+    let game = compMove + '-' + playMove,
+     result = "undefined choice, try again but don't you cheat me...";
+    console.log('computer - player: ' + game);
+
+    if (compMove == playMove){
+      result = "I chose " + compMove + " too, let's play again!";
+      console.log("draw");
+    } else if (game == 'paper-stone' || game == 'scissors-paper' || game == 'stone-scissors'){
+      result = "I have " + compMove + ", you have " + playMove + ". I win, you loose.";
+      compCount += 1;
+      console.log('computer wins - score: ' + compCount);
+      printCountById(compCount, 'comp-count');
+    } else if (game == 'stone-paper' || game == 'paper-scissors' || game == 'scissors-stone'){
+      result = "I have " + compMove + ", you have " + playMove + ". You win this time, let's play again!";
+      playerCount += 1;
+      console.log('player wins - score: ' + playerCount);
+      printCountById(playerCount, 'player-count');
+    }
+    return printMessage(result);
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-function playGame(playerInput){
-  clearMessages();
+{
+  function playGame(playerInput){
+    clearMessages();
 
-  let randomNumber = Math.floor(Math.random()*3+1);
-  let computerMove = getMoveName(randomNumber);
-  let playerMove = getMoveName(playerInput);
+    let randomNumber = Math.floor(Math.random()*3+1);
+    let computerMove = getMoveName(randomNumber);
+    let playerMove = getMoveName(playerInput);
 
-  getResult(computerMove, playerMove);
+    getResult(computerMove, playerMove);
+  }
+
+  document.getElementById('paper-button').addEventListener('click', function(){playGame(1)});
+  document.getElementById('stone-button').addEventListener('click', function(){playGame(2)});
+  document.getElementById('scissors-button').addEventListener('click', function(){playGame(3)});
+
+  document.getElementById('reset-button').addEventListener('click', function(){resetCount()});
 }
-
-document.getElementById('paper-button').addEventListener('click', function(){playGame(1)});
-document.getElementById('stone-button').addEventListener('click', function(){playGame(2)});
-document.getElementById('scissors-button').addEventListener('click', function(){playGame(3)});
-
-document.getElementById('reset-button').addEventListener('click', function(){resetCount()});
